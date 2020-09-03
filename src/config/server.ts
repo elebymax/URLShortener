08f5db42e1326993efedb290e6bc2dcbdf550e53
Lai chane an <m020203@gms.tku.edu.tw>
2020-09-03 15:06:37 +0800

    feat: response completed url after create

--- src/config/server.ts
+++ src/config/server.ts
@@ -4,4 +4,5 @@ dotenv.config()
 export const server = {
   host: process.env.SERVER_HOST,
   port: +(process.env.SERVER_PORT || 3000),
+  domain: process.env.SERVER_DOMAIN,
 }
--- src/url/url.controllers.ts
+++ src/url/url.controllers.ts
@@ -1,12 +1,15 @@
 import { KoaContext } from '../context'
 import * as services from './url.services'
 import * as record from '../record'
+import * as config from '../config'
 
 export const create = async (ctx: KoaContext): Promise<void> => {
   const info = await services.create(ctx, ctx.request.body)
 
   ctx.status = 201
-  ctx.body = info
+  ctx.body = {
+    url: `${config.server.domain}/${info.hash}`,
+  }
 }
 
 export const redirect = async (ctx: KoaContext): Promise<void> => {
